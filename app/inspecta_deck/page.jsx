49597eb3ffc46e6a65112b4089e1407f44584c9b
danyel-ii <222602034+danyel-ii@users.noreply.github.com>
2026-01-19 01:37:50 +0100

    Restore inspecta_deck rewrite

--- app/inspecta_deck/page.jsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { DeckPage } from "../shaolin_deck/DeckPage.jsx";
-
-export default function InspectaDeckPage() {
-  return <DeckPage tokenMode="minter" viewerPath="/m" />;
-}
