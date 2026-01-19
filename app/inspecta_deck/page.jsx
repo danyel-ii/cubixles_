749c9446b7a6ee00b8a03d35c7026bf1dd204125
danyel-ii <222602034+danyel-ii@users.noreply.github.com>
2026-01-19 01:27:28 +0100

    Fix shaolin deck page exports

--- app/inspecta_deck/page.jsx
+++ app/inspecta_deck/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { DeckPage } from "../shaolin_deck/page.jsx";
+import { DeckPage } from "../shaolin_deck/DeckPage.jsx";
 
 export default function InspectaDeckPage() {
   return <DeckPage tokenMode="minter" viewerPath="/m" />;
